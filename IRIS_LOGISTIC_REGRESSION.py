bd16575ece7a550218c36bcd0fb73e7a8c0a45bd
hoda.sadeghian@gmail.com <chichest85%>
2018-10-05 16:44:53 +0330

    Some minor changes

--- IRIS_LOGISTIC_REGRESSION.py
+++ IRIS_LOGISTIC_REGRESSION.py
@@ -1,11 +1,31 @@
 import numpy as np
 from sklearn.datasets import load_iris
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 from matplotlib.patches import FancyArrowPatch
 from mpl_toolkits.mplot3d import Axes3D
 from mpl_toolkits.mplot3d import proj3d
+import imageio
 
+iris_data_set = load_iris()                     # load the iris dataset
+
+x = iris_data_set.data
+y_raw=iris_data_set.target
+y = y_raw.reshape(-1, 1)        # Convert data to a single column
+
+lr=LogisticRegression(C=1e5, solver='lbfgs', multi_class='multinomial')
+
+x_train, x_test, y_train, y_test = train_test_split(
+             x, y, test_size=0.2,
+             random_state=13
+)
+
+lr.fit(x_train, y_train)
+y_pred_lr = lr.predict(x)
+wrong_prediction_lr_indexes=np.asarray(np.where(y_pred_lr.T != y_raw))[0]
+
+# plotting the result for Logistic Regression
 
 class Arrow3D(FancyArrowPatch):
     def __init__(self, xs, ys, zs, *args, **kwargs):
@@ -19,17 +39,6 @@ class Arrow3D(FancyArrowPatch):
         FancyArrowPatch.draw(self, renderer)
 
 
-iris_data_set = load_iris()                     # load the iris dataset
-
-x = iris_data_set.data
-y = iris_data_set.target.reshape(-1, 1)        # Convert data to a single column
-
-lr=LogisticRegression(C=1e5, solver='lbfgs', multi_class='multinomial')
-lr.fit(x, y)
-y_pred_lr = lr.predict(x)
-wrong_prediction_lr_indexes=np.asarray(np.where(y_pred_lr.T != y[:,0]))
-
-# plotting the result for Logistic Regression
 plt.rcParams.update({'font.size': 7})
 labelTups = [(iris_data_set.target_names[0], 0), (iris_data_set.target_names[1], 1), (iris_data_set.target_names[2], 2)]
 fig = plt.figure(figsize=(6, 4))
@@ -39,7 +48,7 @@ arrow_prop_dict = dict(mutation_scale=20, arrowstyle='-|>', color='k', shrinkA=0
 
 for item in wrong_prediction_lr_indexes.T:
     # print(item)
-    y_item=y[item][0][0]
+    y_item=y_raw[item]
     ax.text3D(x[:, 0].mean() + y_item,
               x[:, 1].mean() + 1.5 - y_item,
               x[:, 2].mean(), 'Ground Truth: '+iris_data_set.target_names[y_item],
@@ -65,7 +74,6 @@ fig.tight_layout()
 plt.show()
 
 #animating the result
-import imageio
 def plot_for_offset(angle):
     # Data for plotting
     ax.view_init(30, angle)
